9413879f403182949bb91427744ef518f30da334
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-04 23:29:18 +0300

    refactor: dragndrop component

--- frontend/src/components/dragndrop/DragDropFile.tsx
+++ frontend/src/components/dragndrop/DragDropFile.tsx
@@ -1,6 +1,6 @@
 import { Modal } from "@mui/base/Modal";
 import { Box, styled } from "@mui/system";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 import React from "react";
@@ -11,50 +11,34 @@ export const DragDropFile: React.FC<DragDropFileProps> = () => {
   const [isDragActive, setIsDragActive] = useState(false);
   const [file, setFile] = useState<File | null>(null);
 
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Only accept bpmn files
-    if (acceptedFiles.length > 0 && acceptedFiles[0].name!.endsWith(".bpmn")) {
-      setFile(acceptedFiles[0]);
-    }
-    console.log(file);
-    setIsDragActive(false);
-  }, []);
-
-  useEffect(() => {
-    // Function to handle drag over window
-    const handleDragOver = (e: Event) => {
-      e.preventDefault();
-      setIsDragActive(true);
-    };
-
-    // Function to handle drag leave window
-    const handleDragLeave = () => {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      // Only accept bpmn files
+      if (
+        acceptedFiles.length > 0 &&
+        acceptedFiles[0].name!.endsWith(".bpmn")
+      ) {
+        setFile(acceptedFiles[0]);
+        console.log(file);
+      }
       setIsDragActive(false);
-    };
-
-    // Adding event listeners
-    window.addEventListener("dragover", handleDragOver);
-    window.addEventListener("dragleave", handleDragLeave);
-    window.addEventListener("drop", handleDragLeave);
-
-    // Removing event listeners on component unmount
-    return () => {
-      window.removeEventListener("dragover", handleDragOver);
-      window.removeEventListener("dragleave", handleDragLeave);
-      window.removeEventListener("drop", handleDragLeave);
-    };
-  }, []);
+    },
+    [file]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    noClick: true, // Prevent file dialog on click
-    noKeyboard: true, // Prevent keyboard navigation
+    onDragOver: () => setIsDragActive(true),
+    onDragEnter: () => setIsDragActive(true),
+    onDragLeave: () => setIsDragActive(false),
+    noClick: true,
+    noKeyboard: true,
   });
 
   return (
-    <div
-      style={{
-        display: "none",
+    <DropzoneArea
+      {...getRootProps()}
+      sx={{
         position: "fixed",
         height: "100vh",
         top: 0,
@@ -63,23 +47,21 @@ export const DragDropFile: React.FC<DragDropFileProps> = () => {
         border: "10px solid red",
       }}
     >
+      <input {...getInputProps()} />
       {isDragActive && (
-        <DropzoneArea {...getRootProps()}>
-          <input {...getInputProps()} />
-          <StyledModal
-            open={isDragActive}
-            onClose={() => setIsDragActive(false)}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-          >
-            <StyledBox>
-              <p id="simple-modal-description">Drop files here to upload!</p>
-              <em>(Only *.bmpn files will be accepted)</em>
-            </StyledBox>
-          </StyledModal>
-        </DropzoneArea>
+        <StyledModal
+          open={isDragActive}
+          onClose={() => setIsDragActive(false)}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <StyledBox>
+            <p id="simple-modal-description">Drop files here to upload!</p>
+            <em>(Only *.bmpn files will be accepted)</em>
+          </StyledBox>
+        </StyledModal>
       )}
-    </div>
+    </DropzoneArea>
   );
 };
 
