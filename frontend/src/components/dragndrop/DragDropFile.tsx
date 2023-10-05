9dda0edc4f6ca259a150621c5d862d5bdcc14e8a
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-05 14:49:02 +0300

    refac: fix the flickering modal when dragover

--- frontend/src/components/dragndrop/DragDropFile.tsx
+++ frontend/src/components/dragndrop/DragDropFile.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 import React from "react";
+import { PRIMARY } from "../../theme/palette";
 
 interface DragDropFileProps {}
 
@@ -11,20 +12,14 @@ export const DragDropFile: React.FC<DragDropFileProps> = () => {
   const [isDragActive, setIsDragActive] = useState(false);
   const [file, setFile] = useState<File | null>(null);
 
-  const onDrop = useCallback(
-    (acceptedFiles: File[]) => {
-      // Only accept bpmn files
-      if (
-        acceptedFiles.length > 0 &&
-        acceptedFiles[0].name!.endsWith(".bpmn")
-      ) {
-        setFile(acceptedFiles[0]);
-        console.log(file);
-      }
-      setIsDragActive(false);
-    },
-    [file]
-  );
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Only accept bpmn files
+    if (acceptedFiles.length > 0 && acceptedFiles[0].name!.endsWith(".bpmn")) {
+      setFile(acceptedFiles[0]);
+      console.log(acceptedFiles[0]);
+    }
+    setIsDragActive(false);
+  }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
@@ -44,23 +39,21 @@ export const DragDropFile: React.FC<DragDropFileProps> = () => {
         top: 0,
         width: "100vw",
         boxSizing: "border-box",
-        border: "10px solid red",
       }}
     >
       <input {...getInputProps()} />
-      {isDragActive && (
-        <StyledModal
-          open={isDragActive}
-          onClose={() => setIsDragActive(false)}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-        >
-          <StyledBox>
-            <p id="simple-modal-description">Drop files here to upload!</p>
-            <em>(Only *.bmpn files will be accepted)</em>
-          </StyledBox>
-        </StyledModal>
-      )}
+      <StyledModal
+        sx={{ pointerEvents: "none" }}
+        open={isDragActive}
+        onClose={() => setIsDragActive(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <StyledBox>
+          <p id="simple-modal-description">Drop files here to upload!</p>
+          <em>(Only *.bmpn files will be accepted)</em>
+        </StyledBox>
+      </StyledModal>
     </DropzoneArea>
   );
 };
@@ -76,16 +69,16 @@ const StyledModal = styled(Modal)`
 const DropzoneArea = styled(Box)`
   width: 100%;
   height: 100vh;
-  border: 2px dashed lightgray;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
 `;
+
 const StyledBox = styled(Box)`
   width: 400px;
   height: 200px;
-  border: 3px dashed #97c8eb;
+  border: 3px solid ${PRIMARY[600]};
   border-radius: 8px;
   display: flex;
   flex-direction: column;
@@ -94,11 +87,9 @@ const StyledBox = styled(Box)`
   background-color: #e8f8ff;
   cursor: pointer;
   margin: 16px;
-  &:hover {
-    border-color: #4194e0;
-  }
+
   p {
-    color: #4194e0;
+    color: ${PRIMARY[300]};
     font-size: 16px;
     text-align: center;
   }
