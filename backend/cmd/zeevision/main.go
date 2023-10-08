cbdbd29b17becf5375b9964ff8cde53a9d6cf808
fennekki <fennekki@iki.fi>
2023-10-08 23:33:37 +0300

    feat: Add receiver package
    It currently contains one function, ConsumeStream, that connects to a
    kafka server and pumps messages received into a channel. The main entry
    point uses that to connect to a hardcoded broker, subscribe to hardcoded
    topic in a hardcoded partition, and print the messages received from the
    channel.

--- backend/cmd/zeevision/main.go
+++ backend/cmd/zeevision/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ducanhpham0312/zeevision/backend/internal/receiver"
 )
 
 // Entry point for the application.
 func main() {
-	receiver.SomeData()
+	msgChannel := make(chan []byte)
+	// Launch goroutine for consuming from specified topic and partition
+	brokers := []string{"127.0.0.1:9092"}
+	go receiver.ConsumeStream(brokers, "zeebe-message", 0, msgChannel)
+
+	for {
+		msg := <-msgChannel
+		fmt.Sprintf("Message received: %s\n", msg)
+	}
 }
--- backend/internal/receiver/kafkareceiver.go
+++ backend/internal/receiver/kafkareceiver.go
@@ -1,8 +1,44 @@
 package receiver
 
 import (
+	"log"
+
 	"github.com/IBM/sarama"
 )
 
-func SomeData() {
+func ConsumeStream(addrs []string, topic string, partition int32, msgChannel chan [] byte) {
+	config := sarama.NewConfig()
+
+	// Needs to run on localhost for now vs the docker container.
+	consumer, err := sarama.NewConsumer(addrs, config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	partitionConsumer, err := consumer.ConsumePartition(
+		topic, partition, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	consumed := 0
+	for {
+		msg := <-partitionConsumer.Messages()
+		log.Printf("Consumed message offset %d\n", msg.Offset)
+		log.Printf("value: %s\n", string(msg.Value))
+		consumed++
+		msgChannel <- msg.Value
+	}
 }
