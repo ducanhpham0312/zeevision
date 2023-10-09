77d478b57cec5de5f1aecdecb2e808db741280fd
Elias Kauppi <elkku.kauppi@gmail.com>
2023-10-09 14:26:55 +0300

    feat: initial websocket endpoint

--- backend/cmd/zeevision/main.go
+++ backend/cmd/zeevision/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-	"github.com/ducanhpham0312/zeevision/backend/internal/example"
+	"github.com/ducanhpham0312/zeevision/backend/internal/endpoint"
 )
+
+const DefaultPort = 8080
 
 // Entry point for the application.
 func main() {
-	fmt.Println(example.HelloWorld())
+	// Create default configuration.
+	conf := &endpoint.Config{
+		Port: DefaultPort,
+	}
+
+	// Override configuration with environment variables.
+	if port, ok := os.LookupEnv("PORT"); ok {
+		port, err := strconv.ParseUint(port, 10, 16)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		conf.Port = uint16(port)
+	}
+
+	server, err := endpoint.New(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
