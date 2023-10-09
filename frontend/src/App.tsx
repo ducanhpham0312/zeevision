b955cdcd63c9c4c36ed06062b5fb8b930d8aad54
Elias Kauppi <11910622+kauppie@users.noreply.github.com>
2023-10-10 00:52:16 +0300

    feat: send sample messages via websocket (#107)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import IncidentsPage from "./pages/IncidentsPage";
 import ErrorsPage from "./pages/ErrorsPage";
 import MessagesPage from "./pages/MessagesPage";
 import JobsPage from "./pages/JobsPage";
+import { useEffect } from "react";
 
 const Layout = () => (
   <>
@@ -50,6 +51,26 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const websocket = new WebSocket("ws://127.0.0.1:8080/ws");
+
+  useEffect(() => {
+    websocket.addEventListener("open", () => {
+      console.log("websocket: connected");
+    });
+
+    websocket.addEventListener("close", () => {
+      console.log("websocket: closed");
+    });
+
+    websocket.addEventListener("error", (event) => {
+      console.error("websocket: error:", event);
+    });
+
+    websocket.addEventListener("message", (event) => {
+      console.log("websocket message:", event.data);
+    });
+  });
+
   return <RouterProvider router={router} />;
 }
 
