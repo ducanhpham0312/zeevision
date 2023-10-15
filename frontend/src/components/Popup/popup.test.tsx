b0297f70a3b2df2b05e63bf70064a4b3deb03f1c
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-15 20:09:08 +0300

    optimize test code

--- frontend/src/components/Popup/popup.test.tsx
+++ frontend/src/components/Popup/popup.test.tsx
@@ -4,37 +4,40 @@ import { Popup } from "./Popup";
 import { Button } from "../Button";
 import * as mockdata from "./mockdata.json";
 
+const openPopup = mockdata.openPopup;
+const closedPopup = mockdata.closedPopup;
+
 describe("Popup Component", () => {
   const mockOnClose = jest.fn();
 
   it("renders correctly when open", () => {
     const { baseElement } = render(
-      <Popup {...mockdata.openPopup} onClose={mockOnClose} />
+      <Popup {...openPopup} onClose={mockOnClose} />
     );
     expect(baseElement).toMatchSnapshot();
   });
 
   it("does not render when closed", () => {
     const { baseElement } = render(
-      <Popup {...mockdata.closedPopup} onClose={mockOnClose} />
+      <Popup {...closedPopup} onClose={mockOnClose} />
     );
     expect(baseElement).toMatchSnapshot();
   });
 
   it("calls onClose when close button is clicked", () => {
     const onClose = jest.fn();
-    render(<Popup {...mockdata.openPopup} onClose={onClose} />);
+    render(<Popup {...openPopup} onClose={onClose} />);
     fireEvent.click(screen.getByRole("button"));
     expect(onClose).toHaveBeenCalledTimes(1);
   });
 
   it("renders the title correctly", () => {
-    render(<Popup {...mockdata.openPopup} onClose={mockOnClose} />);
+    render(<Popup {...openPopup} onClose={mockOnClose} />);
     expect(screen.getByText("Test Title")).toBeInTheDocument();
   });
 
   it("renders children correctly", () => {
-    render(<Popup {...mockdata.openPopup} onClose={mockOnClose} />);
+    render(<Popup {...openPopup} onClose={mockOnClose} />);
     expect(screen.getByText(/Test child 1/)).toBeInTheDocument();
     expect(screen.getByText(/Test child 2/)).toBeInTheDocument();
   });
@@ -42,7 +45,7 @@ describe("Popup Component", () => {
   it("renders a button with correct text and form of Button Component inside the Popup", () => {
     const buttonText = "Click Me!";
     render(
-      <Popup {...mockdata.openPopup}>
+      <Popup {...openPopup}>
         <p>Test child 1</p>
         <p>Test child 2</p>
         <Button>{buttonText}</Button>
@@ -53,7 +56,7 @@ describe("Popup Component", () => {
   });
 
   it("Backdrop displays correctly when the popup is open", () => {
-    render(<Popup {...mockdata.openPopup} onClose={mockOnClose} />);
+    render(<Popup {...openPopup} onClose={mockOnClose} />);
     const backdrop = screen.getByRole("presentation");
     expect(backdrop).toHaveStyle({
       position: "fixed",
