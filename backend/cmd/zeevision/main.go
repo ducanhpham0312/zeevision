12bdfe3dd506de5f2e0a279606bc83f2af44efb1
Elias Kauppi <elkku.kauppi@gmail.com>
2023-10-17 13:29:14 +0300

    Revert "feat: use nginx as reverse proxy and server (#122)"
    This reverts commit 2542c27d1d1c3b1c055cc685a3d96a2c0f3f82ca.

--- backend/cmd/zeevision/main.go
+++ backend/cmd/zeevision/main.go
@@ -10,24 +10,14 @@ import (
 	"github.com/ducanhpham0312/zeevision/backend/internal/endpoint"
 )
 
-const DefaultWebsocketPort = 8080
-const DefaultKafkaAddr = "localhost:9092"
+const DefaultPort = 8080
 
 // Entry point for the application.
 func main() {
-	// Lookup address for Kafka broker.
-	kafkaAddr, ok := os.LookupEnv("KAFKA_ADDR")
-	if !ok {
-		log.Println("KAFKA_ADDR not set; using default address")
-		kafkaAddr = DefaultKafkaAddr
-	}
-	log.Printf("Listening for Kafka at %s\n", kafkaAddr)
-
 	msgChannel := make(chan []byte)
-
 	// Launch goroutine for consuming from specified topic and partition
-	brokers := []string{kafkaAddr}
-	go consumer.ConsumeStream(brokers, "zeebe-deployment", 0, msgChannel)
+	brokers := []string{"127.0.0.1:9092"}
+	go consumer.ConsumeStream(brokers, "zeebe-message", 0, msgChannel)
 
 	go func() {
 		for {
@@ -38,7 +28,7 @@ func main() {
 
 	// Create default configuration.
 	conf := &endpoint.Config{
-		Port: DefaultWebsocketPort,
+		Port: DefaultPort,
 	}
 
 	// Override configuration with environment variables.
