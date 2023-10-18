a3196ade5a5edba97d7d62a408db953d37c73cd2
Elias Kauppi <elkku.kauppi@gmail.com>
2023-10-18 15:44:14 +0300

    fix: don't use host network

--- backend/cmd/zeevision/main.go
+++ backend/cmd/zeevision/main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/ducanhpham0312/zeevision/backend/internal/consumer"
 	"github.com/ducanhpham0312/zeevision/backend/internal/endpoint"
 )
 
+const (
+	// Default Kafka address to use.
+	DefaultKafkaAddr = "kafka:9093"
+
+	// Environment variable used to configure the Kafka address.
+	EnvVarKafkaAddr = "ZEEVISION_KAFKA_ADDR"
+)
+
 // Entry point for the application.
 func main() {
+	// Get Kafka address from environment variable.
+	kafkaAddr := DefaultKafkaAddr
+	if envKafkaAddr, ok := os.LookupEnv(EnvVarKafkaAddr); ok {
+		kafkaAddr = envKafkaAddr
+	}
+
 	msgChannel := make(chan []byte)
 	// Launch goroutine for consuming from specified topic and partition
-	brokers := []string{"127.0.0.1:9092"}
+	brokers := []string{kafkaAddr}
 	go consumer.ConsumeStream(brokers, "zeebe-message", 0, msgChannel)
 
 	go func() {
