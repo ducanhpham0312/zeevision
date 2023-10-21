2c43e32caf1c073115b63acdf5ae1a28653e10e9
Duc Anh Pham <77518288+ducanhpham0312@users.noreply.github.com>
2023-10-21 23:09:24 +0300

    feat: implement table component (#127)

---
+++ frontend/src/components/Table/table.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Table } from "./Table";
+import mockdata from "./mockdata.json";
+
+const meta = {
+  title: "Assets/Table",
+  component: Table,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Table>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Horizontal: Story = {
+  args: {
+    orientation: "horizontal",
+    header: mockdata.horizontal.headers,
+    content: mockdata.horizontal.content,
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    orientation: "vertical",
+    header: mockdata.vertical.headers,
+    content: mockdata.vertical.content,
+  },
+};
