f83892f235d5c0bc95ee5674029613365e9d5580
fennekki <fennekki@iki.fi>
2023-10-25 05:01:24 +0300

    Change back to single channel
    We might just end up designing the storage API such that this is
    unnecessary and therefore we can avoid the complexity of having several
    of them

--- backend/internal/consumer/kafka_consumer.go
+++ backend/internal/consumer/kafka_consumer.go
@@ -9,6 +9,15 @@ import (
 
 type channelType chan []byte
 
+// TODO: we need a method of reconnecting when the connection drops. I'm not
+// entirely sure how to *detect* this, except perhaps by adding a condition
+// variable of some kind to the Consumer object where, upon seeing it, a
+// housekeeping goroutine would proceed to close down and restart all the
+// topics (since we keep track of every topic this would not be excessively
+// difficult). The housekeeping goroutine could also be in charge of retrying
+// and should close down when the externally visible Close method is called (so
+// we should have an internal close method it calls to clean up after
+// signalling the housekeeping goroutine to close first)
 // TODO: we likely need synchronisation on at least some of the methods of this
 // struct... or a restriction on calling any of them from outside the main
 // goroutine?
@@ -16,8 +25,8 @@ type Consumer struct {
 	brokers []string
 	topics  []string
 
-	consumer      sarama.Consumer
-	topicChannels map[string]channelType
+	consumer   sarama.Consumer
+	msgChannel channelType
 
 	wg           *sync.WaitGroup
 	closeChannel chan bool
@@ -31,13 +40,19 @@ func NewConsumer(brokers []string) (*Consumer, error) {
 		return nil, err
 	}
 
+	// TODO: buffer channel sufficiently so we don't *usually* end up
+	// dropping anything or blocking too much?
+	// Depending on what the storage API looks like the channel might not
+	// be needed in the end (could be wrapped by a storage call)
+	bufSize := 10
+
 	var wg sync.WaitGroup
 	result := Consumer{
 		brokers: brokers,
 		topics:  []string{},
 
-		consumer:      consumer,
-		topicChannels: map[string]channelType{},
+		consumer:   consumer,
+		msgChannel: make(channelType, bufSize),
 
 		wg:           &wg,
 		closeChannel: make(chan bool),
@@ -56,13 +71,7 @@ func (consumer Consumer) ConsumeTopic(partition int32, topic string) (msgChannel
 	if err != nil {
 		return
 	}
-
-	// TODO: buffer channel sufficiently so we don't *usually* end up
-	// dropping anything or blocking too much?
-	bufSize := 10
-	msgChannel = make(channelType, bufSize)
-	consumer.topicChannels[topic] = msgChannel
-
+	msgChannel = consumer.msgChannel
 	closeChannel := consumer.closeChannel
 	wg := consumer.wg
 
@@ -102,11 +111,11 @@ func (consumer Consumer) ConsumeTopic(partition int32, topic string) (msgChannel
 					// should be part of the main select?
 					// just need to figure out how to pass
 					// the messages there. an internal channel?)
-					log.Printf("Dropping value on the floor (no reader)")
+					log.Printf("Dropping value on the floor (would block)")
 					log.Printf("value: %s\n", string(msg.Value))
 				}
-			// No default case so we will simply wait when there's
-			// nothing to do
+				// No default case so we will simply wait when there's
+				// nothing to do
 			}
 		}
 	}()
@@ -114,7 +123,12 @@ func (consumer Consumer) ConsumeTopic(partition int32, topic string) (msgChannel
 	return
 }
 
-// TODO: func (consumer Consumer) GetChannel(topic string) channelType
+// TODO: func (consumer Consumer) GetChannel() channelType
+// except that we're actually going to be passing data to our storage/database
+// layer which will pass it on - even streaming wouldn't go directly from here,
+// probably
+// If another layer wants to have an internal channel they can implement that
+// there. The channel is just kind of a hack!
 
 func (consumer Consumer) Close() error {
 	// Close the partitionconsumers (they will simply log any errors when
