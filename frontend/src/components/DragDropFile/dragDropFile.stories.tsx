92b04f17b2d0049c001c2512fd6004524e9b9386
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-27 00:33:55 +0300

    feat: storybook name of DragDropFile

--- frontend/src/components/DragDropFile/dragDropFile.stories.tsx
+++ frontend/src/components/DragDropFile/dragDropFile.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { DragDropFile, StyledBox, StyledModal } from "./DragDropFile";
 
 const meta = {
-  title: "Assets/DragDrop",
+  title: "Assets/DragDropFile",
   component: DragDropFile,
   parameters: {
     layout: "centered",
