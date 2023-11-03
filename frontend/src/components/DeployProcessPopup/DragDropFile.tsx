f31025778ef2303f5a84bc0c92f8f7531910cb87
quyenkhanhnghi <108847306+quyenkhanhnghi@users.noreply.github.com>
2023-11-03 12:26:58 +0200

    feat: read bpmn files from user input (#140)

---
+++ frontend/src/components/DeployProcessPopup/DragDropFile.tsx
@@ -0,0 +1,84 @@
+import { Modal } from "@mui/base/Modal";
+import { Box, styled } from "@mui/system";
+
+import { useEffect } from "react";
+import { PRIMARY } from "../../theme/palette";
+import { useDragDrop } from "../../hooks/useDragDrop";
+import { readFileToString } from "../../utils/readFileToString";
+import { useUIStore } from "../../contexts/useUIStore";
+
+interface DragDropFileProps {
+  /**
+   * Callback function that is triggered when a file is dropped.
+   *
+   * @param fileContent - The content of the dropped file as a string.
+   */
+  onFileDropped: (fileContent: string) => void;
+}
+
+export function DragDropFile({ onFileDropped }: DragDropFileProps) {
+  const { dragging, file } = useDragDrop();
+  const { setSnackbarContent } = useUIStore();
+
+  useEffect(() => {
+    const setFile = async () => {
+      if (file) {
+        if (file.name.endsWith(".bpmn")) {
+          onFileDropped(await readFileToString(file));
+        } else {
+          setSnackbarContent({
+            title: "Invalid file type",
+            message: "The file was not .bpmn file",
+            type: "error",
+          });
+        }
+      }
+    };
+
+    setFile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [file]);
+
+  return (
+    <StyledModal
+      sx={{ pointerEvents: "none" }}
+      open={dragging}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <StyledBox>
+        <p id="simple-modal-description">Drop files here to upload!</p>
+        <em>(Only *.bmpn files will be accepted)</em>
+      </StyledBox>
+    </StyledModal>
+  );
+}
+
+export const StyledModal = styled(Modal)`
+  position: fixed;
+  z-index: 1300;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledBox = styled(Box)`
+  width: 400px;
+  height: 200px;
+  border: 3px solid ${PRIMARY[600]};
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #e8f8ff;
+  cursor: pointer;
+  margin: 16px;
+
+  p {
+    color: ${PRIMARY[300]};
+    font-size: 16px;
+    text-align: center;
+  }
+`;
