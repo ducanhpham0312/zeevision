d2ab9da2ae8ef4f38bcb70e9d1764dadd6897463
ducanhpham <ducanh.pham@elisa.fi>
2023-11-11 02:19:52 +0200

    style: ignore gomnd

--- backend/graph/dummy.go
+++ backend/graph/dummy.go
@@ -4,11 +4,13 @@ import "github.com/ducanhpham0312/zeevision/backend/graph/model"
 
 var dummyInstances = []*model.Instance{
 	{
+		//nolint:gomnd
 		InstanceKey: 12345,
 		Status:      "Active",
 		StartTime:   "2023-01-02T00:00:00Z",
 	},
 	{
+		//nolint:gomnd
 		InstanceKey: 54321,
 		Status:      "completed",
 		StartTime:   "2023-01-01T00:15:00Z",
