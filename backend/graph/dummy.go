5ba74adb910e993a166afabc0c13972bdfdce4e7
ducanhpham <ducanh.pham@elisa.fi>
2023-11-11 02:16:24 +0200

    style: backend lint

--- backend/graph/dummy.go
+++ backend/graph/dummy.go
@@ -1,16 +1,17 @@
 package graph
 
 import "github.com/ducanhpham0312/zeevision/backend/graph/model"
+
 var dummyInstances = []*model.Instance{
 	{
-		InstanceKey: 			12345,
-		Status:						"Active",
-		StartTime:   			"2023-01-02T00:00:00Z",
+		InstanceKey: 12345,
+		Status:      "Active",
+		StartTime:   "2023-01-02T00:00:00Z",
 	},
 	{
-		InstanceKey: 			54321,
-		Status:						"completed",
-		StartTime:   			"2023-01-01T00:15:00Z",
+		InstanceKey: 54321,
+		Status:      "completed",
+		StartTime:   "2023-01-01T00:15:00Z",
 	},
 }
 var dummyProcesses = []*model.Process{
