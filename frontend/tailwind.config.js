e91cd6fbf21e107947d0cbf600742a2e9e18787d
Duy Tran <trananhduy041003@gmail.com>
2023-11-13 21:57:29 +0200

    feat(client): some constant color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const PRIMARY = {
+  50: "#f6f8ff",
+  100: "#cfe6ff",
+  200: "#a9cfff",
+  300: "#76adff",
+  400: "#4078ff",
+  500: "#1645ff",
+  600: "#0029ff",
+  700: "#002cff",
+  800: "#0027e1",
+  900: "#0119af",
+  950: "#010c65",
+};
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hover: PRIMARY[50],
+        active: PRIMARY[100],
+        text: PRIMARY[900],
+        background: "#fff",
+        accent: PRIMARY[900],
+      },
+    },
   },
   plugins: [],
-}
-
+};
