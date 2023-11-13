d3949d81fd740d87b12add54fd51f2757a897ffb
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-11-13 15:36:37 +0200

    feat: add test nav bar and command run cypress

---
+++ frontend/cypress/e2e/navbar.cy.ts
@@ -0,0 +1,23 @@
+describe("Navbar Tests", () => {
+  const pages = [
+    "/", // Home page
+    "/processes", // Process Page
+    "/instances", // Instances Page
+    "/incidents", // Incidents Page
+    "/jobs", // Jobs Page
+    "/messages", // Messages Page
+    "/errors", // Errors Page
+  ];
+
+  beforeEach(() => {
+    cy.visit("/"); // Start at the home page for each test
+  });
+
+  pages.forEach((page) => {
+    it(`should have a navbar on the ${page} page`, () => {
+      cy.visit(page); // Navigate to the page
+      cy.get("header").contains("NAVBAR").should("exist");
+      cy.screenshot(`Navbar${page}`); // Screenshot for each page
+    });
+  });
+});
