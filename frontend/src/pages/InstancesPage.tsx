af091455ffabeeecce135dcc32e96bbf37532ac0
ducanhpham <ducanh.pham@elisa.fi>
2023-11-16 00:07:40 +0200

    feat: add Table to InstancesPage

--- frontend/src/pages/InstancesPage.tsx
+++ frontend/src/pages/InstancesPage.tsx
@@ -1,20 +1,50 @@
-import { useState } from "react";
-import { Button } from "../components/Button";
-import { DeployProcessPopup } from "../components/DeployProcessPopup";
+import { gql, useQuery } from "@apollo/client";
+import { Table } from "../components/Table";
 export default function InstancesPage() {
-  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
-  const handleOpen = () => setIsPopUpOpen(true);
-  const handleClose = () => setIsPopUpOpen(false);
+  const INSTANCES = gql`
+    query Instances {
+      instances {
+        instanceKey
+        processId
+        status
+        startTime
+      }
+    }
+  `
+  const { data } = useQuery(INSTANCES)
+  // const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+  // const handleOpen = () => setIsPopUpOpen(true);
+  // const handleClose = () => setIsPopUpOpen(false);
   return (
-    <>
-      <h1>InstancesPage</h1>
-      {/** Temporary test for open Modal */}
-      <Button onClick={handleOpen}>Deploy a Process</Button>
-      <DeployProcessPopup
-        isPopUpOpen={isPopUpOpen}
-        onOpenPopUp={handleOpen}
-        onClosePopUp={handleClose}
-      />
-    </>
+    // <>
+    //   {/** Temporary test for open Modal. Must be moved to ProcessesPage after issue #94 is completed */}
+    //   <Button onClick={handleOpen}>Deploy a Process</Button>
+    //   <DeployProcessPopup
+    //     isPopUpOpen={isPopUpOpen}
+    //     onOpenPopUp={handleOpen}
+    //     onClosePopUp={handleClose}
+    //   />
+    // </>
+    <Table 
+      orientation="horizontal" 
+      header={["Instance Key", "BPMN Process ID", "Status", "Start Time"]}
+      content={
+        data
+          ? data.instances.map(
+              ({
+                instanceKey,
+                processId,
+                status,
+                startTime,
+              }: {
+                instanceKey: number;
+                processId: string;
+                status: string;
+                startTime: string;
+              }) => [instanceKey,processId, status, startTime]
+            )
+          : []
+      }
+    />
   );
 }
