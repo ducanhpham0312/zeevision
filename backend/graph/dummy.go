6e2fe00c334b71fa7649adf5ba6ea3c703a96adf
ducanhpham <ducanh.pham@elisa.fi>
2023-11-16 00:42:01 +0200

    lint: gofmt

--- backend/graph/dummy.go
+++ backend/graph/dummy.go
@@ -6,21 +6,21 @@ var dummyInstances = []*model.Instance{
 	{
 		//nolint:gomnd
 		InstanceKey: 12345,
-		ProcessID: 	 "multi-instance-process",
+		ProcessID:   "multi-instance-process",
 		Status:      "Active",
 		StartTime:   "2023-01-02T00:00:00Z",
 	},
 	{
 		//nolint:gomnd
 		InstanceKey: 54321,
-		ProcessID: 	 "money-loan",
+		ProcessID:   "money-loan",
 		Status:      "Completed",
 		StartTime:   "2023-01-01T00:15:00Z",
 	},
 	{
 		//nolint:gomnd
 		InstanceKey: 55555,
-		ProcessID: 	 "order-main",
+		ProcessID:   "order-main",
 		Status:      "Completed",
 		StartTime:   "2023-01-08T00:15:00Z",
 	},
