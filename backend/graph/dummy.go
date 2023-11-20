8ad72c1751a5e25d80d205faa401351b1156ae4a
ducanhpham <ducanh.pham@elisa.fi>
2023-11-20 23:39:43 +0200

    style: gofmt backend

--- backend/graph/dummy.go
+++ backend/graph/dummy.go
@@ -5,23 +5,23 @@ import "github.com/ducanhpham0312/zeevision/backend/graph/model"
 var dummyInstances = []*model.Instance{
 	{
 		//nolint:gomnd
-		InstanceKey: 12345,
-		BpmnProcessID:   "multi-instance-process",
-		Status:      "Active",
-		StartTime:   "2023-01-02T00:00:00Z",
+		InstanceKey:   12345,
+		BpmnProcessID: "multi-instance-process",
+		Status:        "Active",
+		StartTime:     "2023-01-02T00:00:00Z",
 	},
 	{
 		//nolint:gomnd
-		InstanceKey: 54321,
-		BpmnProcessID:   "money-loan",
-		Status:      "Completed",
-		StartTime:   "2023-01-01T00:15:00Z",
+		InstanceKey:   54321,
+		BpmnProcessID: "money-loan",
+		Status:        "Completed",
+		StartTime:     "2023-01-01T00:15:00Z",
 	},
 	{
 		//nolint:gomnd
-		InstanceKey: 55555,
-		BpmnProcessID:   "order-main",
-		Status:      "Completed",
-		StartTime:   "2023-01-08T00:15:00Z",
+		InstanceKey:   55555,
+		BpmnProcessID: "order-main",
+		Status:        "Completed",
+		StartTime:     "2023-01-08T00:15:00Z",
 	},
 }
