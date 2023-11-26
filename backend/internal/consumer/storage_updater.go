c10061cd493e49ae7a7e3cd28e0b430851c0c194
fennekki <fennekki@iki.fi>
2023-11-26 10:31:01 +0200

    Start handling instances, variables
    This works-ish, now, and the data reaches the frontend. The problem we
    get into is that we can have a data race between the creation of
    instances and variables, which needs some sort of retry mechanism. One
    way to do this might be to use a pool of goroutines all running the
    storageUpdaterLoop (enough that we should never run out of them before
    we manage to resolve a dependency loop)

--- backend/internal/consumer/storage_updater.go
+++ backend/internal/consumer/storage_updater.go
@@ -72,7 +72,18 @@ readLoop:
 					log.Printf("Failed to handle process: %v", err)
 					continue readLoop
 				}
-
+			case ValueTypeProcessInstance:
+				err = u.handleProcessInstance(&untypedRecord)
+				if err != nil {
+					log.Printf("Failed to handle process instance: %v", err)
+					continue readLoop
+				}
+			case ValueTypeVariable:
+				err = u.handleVariable(&untypedRecord)
+				if err != nil {
+					log.Printf("Failed to handle variable: %v", err)
+					continue readLoop
+				}
 			default:
 				log.Printf("Unhandled record: %v (intent: %v)",
 					untypedRecord.ValueType, untypedRecord.Intent)
@@ -130,6 +141,9 @@ func (u *storageUpdater) handleDeployment(untypedRecord *UntypedRecord) error {
 
 		// We'll also get IntentFullyDistributed once it's distributed to all
 		// zeebe partitions but I'm not sure that's useful information to us
+	default:
+		log.Printf("Unhandled intent for %v: %s",
+			record.ValueType, record.Intent)
 	}
 
 	// If we get here we did nothing or missed all err returns so handling
@@ -143,9 +157,136 @@ func (u *storageUpdater) handleProcess(untypedRecord *UntypedRecord) error {
 		return fmt.Errorf("failed to cast: %w", err)
 	}
 
+	processDefinitionKey := record.Value.ProcessDefinitionKey
+	bpmnProcessID := record.Value.BpmnProcessID
 	switch record.Intent {
 	case IntentCreated:
-		fmt.Printf("Record: %v", record)
+		// I think these should already be handled in deploy?
+		// Log them here anyway in case that's *not* always the case
+		log.Printf("Process created: %d (%s)",
+			processDefinitionKey, bpmnProcessID)
+	default:
+		log.Printf("Unhandled intent for %v: %s",
+			record.ValueType, record.Intent)
+	}
+
+	// If we get here we did nothing or missed all err returns so handling
+	// succeeded
+	return nil
+}
+
+func (u *storageUpdater) handleProcessInstance(untypedRecord *UntypedRecord) error {
+	storer := u.storer
+
+	record, err := WithTypedValue[ProcessInstanceValue](*untypedRecord)
+	if err != nil {
+		return fmt.Errorf("failed to cast: %w", err)
+	}
+
+	// bpmnProcessID := record.Value.BpmnProcessID
+	processInstanceKey := record.Value.ProcessInstanceKey
+	processDefinitionKey := record.Value.ProcessDefinitionKey
+	// elementID := record.Value.ElementID
+	// bpmnEventType := record.Value.BpmnEventType
+	// parentProcessInstanceKey := record.Value.ParentProcessInstanceKey
+	// parentElementInstanceKey := record.Value.ParentElementInstanceKey
+	bpmnElementType := record.Value.BpmnElementType
+	version := record.Value.Version
+
+	// log.Printf("intent %v; process id %s, instance key %d, definition key %d, element id %s, event type %s, parent instance key %d, parent element instance key %d, element type %v",
+	// 	record.Intent,
+	// 	bpmnProcessID,
+	// 	processInstanceKey,
+	// 	processDefinitionKey,
+	// 	elementID,
+	// 	bpmnEventType,
+	// 	parentProcessInstanceKey,
+	// 	parentElementInstanceKey,
+	// 	bpmnElementType,
+	// )
+	switch record.Intent {
+	case IntentElementActivating:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance activating: %d", processInstanceKey)
+		}
+	case IntentElementActivated:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance activated: %d", processInstanceKey)
+			return storer.ProcessInstanceActivated(
+				processInstanceKey,
+				processDefinitionKey,
+				version,
+				time.UnixMilli(record.Timestamp),
+			)
+		}
+	case IntentElementCompleting:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance completing: %d", processInstanceKey)
+		}
+	case IntentElementCompleted:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance completed: %d", processInstanceKey)
+			return storer.ProcessInstanceCompleted(
+				processInstanceKey,
+				time.UnixMilli(record.Timestamp),
+			)
+		}
+	case IntentElementTerminating:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance completing: %d", processInstanceKey)
+		}
+	case IntentElementTerminated:
+		if bpmnElementType == "PROCESS" {
+			log.Printf("Process instance completed: %d", processInstanceKey)
+			// TODO we need a status for this
+		}
+	default:
+		log.Printf("Unhandled intent for %v: %s",
+			record.ValueType, record.Intent)
+	}
+
+	// If we get here we did nothing or missed all err returns so handling
+	// succeeded
+	return nil
+}
+
+func (u *storageUpdater) handleVariable(untypedRecord *UntypedRecord) error {
+	storer := u.storer
+
+	record, err := WithTypedValue[VariableValue](*untypedRecord)
+	if err != nil {
+		return fmt.Errorf("failed to cast: %w", err)
+	}
+
+	processInstanceKey := record.Value.ProcessInstanceKey
+	name := record.Value.Name
+	value := record.Value.Value
+	switch record.Intent {
+	case IntentCreated:
+		// I think these should already be handled in deploy?
+		// Log them here anyway in case that's *not* always the case
+		log.Printf("Variable created: %s = %s (instance %d)",
+			name, value, processInstanceKey)
+		return storer.VariableCreated(
+			processInstanceKey,
+			name,
+			value,
+			time.UnixMilli(record.Timestamp),
+		)
+	case IntentUpdated:
+		// I think these should already be handled in deploy?
+		// Log them here anyway in case that's *not* always the case
+		log.Printf("Variable updated: %s = %s (instance %d)",
+			name, value, processInstanceKey)
+		return storer.VariableUpdated(
+			processInstanceKey,
+			name,
+			value,
+			time.UnixMilli(record.Timestamp),
+		)
+	default:
+		log.Printf("Unhandled intent for %v: %s",
+			record.ValueType, record.Intent)
 	}
 
 	// If we get here we did nothing or missed all err returns so handling
--- backend/internal/storage/store.go
+++ backend/internal/storage/store.go
@@ -8,6 +8,8 @@ import (
 	"gorm.io/gorm"
 )
 
+// TODO: use context for queries where reasonable
+
 // TODO more useful name
 type Storer struct {
 	db *gorm.DB
@@ -30,17 +32,110 @@ func (r *Storer) ProcessDeployed(
 		BpmnFile:      base64.StdEncoding.EncodeToString(bpmnResourceRaw),
 	}
 
-	result := r.db.Create(&Process{
+	err := r.db.Create(&Process{
 		ProcessDefinitionKey: processDefinitionKey,
 		BpmnProcessID:        bpmnProcessID,
 		Version:              version,
 		DeploymentTime:       deploymentTime,
 		BpmnResource:         bpmnResource,
-	})
-
-	err := result.Error
+	}).Error
 	if err != nil {
 		return fmt.Errorf("failed to create process: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Storer) ProcessInstanceActivated(
+	processInstanceKey int64,
+	processDefinitionKey int64,
+	version int64,
+	startTime time.Time,
+) error {
+	// TODO set EndTime to NULL perhaps?
+	err := r.db.Create(&Instance{
+		ProcessInstanceKey:   processInstanceKey,
+		ProcessDefinitionKey: processDefinitionKey,
+		Version:              version,
+		Status:               "ACTIVE",
+		StartTime:            startTime,
+	}).Error
+	if err != nil {
+		return fmt.Errorf("failed to create process instance: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Storer) ProcessInstanceCompleted(
+	processInstanceKey int64,
+	endTime time.Time,
+) error {
+	var instance Instance
+	err := r.db.
+		Where(&Instance{ProcessInstanceKey: processInstanceKey}).
+		First(&instance).Error
+	if err != nil {
+		return fmt.Errorf("failed to find process instance: %w", err)
+	}
+
+	err = r.db.Model(&instance).
+		Select("Status", "EndTime").
+		Updates(Instance{
+			Status:  "COMPLETED",
+			EndTime: endTime,
+		}).Error
+	if err != nil {
+		return fmt.Errorf("failed to update instance: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Storer) VariableCreated(
+	processInstanceKey int64,
+	name string,
+	value string,
+	time time.Time,
+) error {
+	err := r.db.Create(&Variable{
+		ProcessInstanceKey: processInstanceKey,
+		Name: name,
+		Value: value,
+		Time: time,
+	}).Error
+	if err != nil {
+		return fmt.Errorf("failed to create variable: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Storer) VariableUpdated(
+	processInstanceKey int64,
+	name string,
+	value string,
+	time time.Time,
+) error {
+	var variable Variable
+	err := r.db.
+		Where(&Variable{
+			ProcessInstanceKey: processInstanceKey,
+			Name: name,
+		}).
+		First(&variable).Error
+	if err != nil {
+		return fmt.Errorf("failed to find process instance: %w", err)
+	}
+
+	err = r.db.Model(&variable).
+		Select("Value", "Time").
+		Updates(&Variable{
+			Value: value,
+			Time: time,
+		}).Error
+	if err != nil {
+		return fmt.Errorf("failed to save variable: %w", err)
 	}
 
 	return nil
