cbdc3115911803972e2403954910872f2b9501c4
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-11-30 18:31:15 +0200

    feat: fix the view mock constructor and add tests

--- frontend/src/components/BpmnViewer/bpmnViewer.test.tsx
+++ frontend/src/components/BpmnViewer/bpmnViewer.test.tsx
@@ -1,95 +1,82 @@
-import '@testing-library/jest-dom';
-import { render, waitFor } from '@testing-library/react';
-import { BpmnViewer } from './BpmnViewer';
-// import NavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
-// import Viewer from 'bpmn-js/lib/Viewer';
-// import { mocked } from 'jest-mock';
+import "@testing-library/jest-dom";
+import { render, waitFor } from "@testing-library/react";
+import NavigatedViewer from "bpmn-js/lib/NavigatedViewer";
+import { BpmnViewer } from "./BpmnViewer";
+import Viewer from "bpmn-js/lib/Viewer";
 
-interface ViewerInterface {
-  importXML: (xml: string) => Promise<any>;
-  destroy: () => void;
-  get: (module: string) => any;
-}
-
-jest.mock('bpmn-js/lib/NavigatedViewer', () => {
-  class MockNavigatedViewer implements ViewerInterface {
-    options: Record<string, unknown>;
-    constructor(options: Record<string, unknown>) {
-    this.options = options}
-
-    importXML = jest.fn().mockResolvedValue({ warnings: [] });
-    destroy = jest.fn();
-    get = jest.fn().mockReturnValue({
-      viewbox: () => ({ inner: {}, outer: {} }),
-      zoom: jest.fn(),
-    });
-    // Expose the mock function for tests
-  }
-  return { default: MockNavigatedViewer }
+// Mock class constructor
+jest.mock("bpmn-js/lib/NavigatedViewer", () => {
+  return {
+    __esModule: true, // ESModule compatibility
+    default: jest.fn().mockImplementation(() => {
+      return {
+        // Mock methods and properties
+        importXML: jest.fn(),
+        destroy: jest.fn(),
+        get: jest.fn().mockReturnValue({
+          viewbox: () => ({ inner: {}, outer: {} }),
+          zoom: jest.fn(),
+        }),
+      };
+    }),
+  };
 });
 
-jest.mock('bpmn-js/lib/Viewer', () => {
-  class MockViewer implements ViewerInterface {
-    options: Record<string, unknown>;
-    constructor(options: Record<string, unknown>) {
-    this.options = options}
-  
-    importXML = jest.fn().mockResolvedValue({ warnings: [] });
-    destroy = jest.fn();
-    get = jest.fn().mockReturnValue({
-      viewbox: () => ({ inner: {}, outer: {} }),
-      zoom: jest.fn(),
-    });
-  }
-  return { default: MockViewer };
+jest.mock("bpmn-js/lib/Viewer", () => {
+  return {
+    __esModule: true, // ESModule compatibility
+    default: jest.fn().mockImplementation(() => {
+      return {
+        // Mock methods and properties
+        importXML: jest.fn(),
+        destroy: jest.fn(),
+        get: jest.fn().mockReturnValue({
+          viewbox: () => ({ inner: {}, outer: {} }),
+          zoom: jest.fn(),
+        }),
+      };
+    }),
+  };
 });
 
-
-describe('BpmnViewer Component', () => {
-  it('should match the snapshot', () => {
-    const bpmnString = 'some-bpmn-xml-string';
-    const { asFragment } = render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
+describe("BpmnViewer Component", () => {
+  it("correctly render the snapshot", () => {
+    const bpmnString = "some-bpmn-xml-string";
+    const { asFragment } = render(
+      <BpmnViewer bpmnString={bpmnString} navigated={true} />,
+    );
 
     expect(asFragment()).toMatchSnapshot();
   });
-  it('renders and initializes bpmn-js viewer', async () => {
-    const bpmnString = 'some-bpmn-xml-string';
-    
+
+  it("renders and initializes bpmn-js viewer", async () => {
+    const bpmnString = "some-bpmn-xml-string";
+
     // Render the component
-    const { getByTestId } = render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
+    const { getByTestId } = render(
+      <BpmnViewer bpmnString={bpmnString} navigated={true} />,
+    );
 
     await waitFor(() => {
-      expect(getByTestId('canvas')).toBeInTheDocument();
+      expect(getByTestId("canvas")).toBeInTheDocument();
     });
   });
 
-  // it('uses NavigatedViewer when navigated prop is true', async () => {
-  //   const bpmnString = 'some-bpmn-xml-string';
-  //   render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
-  
-  //   await waitFor(() => {
-  //     // Ensure the constructor for NavigatedViewer was called
-  //     const mockedNavigatedViewer = mocked(NavigatedViewer, true);
-  //     expect(mockedNavigatedViewer).toHaveBeenCalledTimes(1);
-  //   });
-  // });
+  it("uses NavigatedViewer when navigated prop is true", async () => {
+    const bpmnString = "some-bpmn-xml-string";
+    render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
 
-  // it('uses Viewer when navigated prop is false', async () => {
-  //   const bpmnString = 'some-bpmn-xml-string';
-  //   render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
-  
-  //   await waitFor(() => {
-  //     expect(Viewer).toHaveBeenCalled();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(NavigatedViewer).toHaveBeenCalled();
+    });
+  });
 
-  // it('imports BPMN string correctly', async () => {
-  //   const bpmnString = 'some-bpmn-xml-string';
-  //   render(<BpmnViewer bpmnString={bpmnString} navigated={true} />);
-  
-  //   await waitFor(() => {
-  //     const mockNavigatedViewer = NavigatedViewer as jest.MockedClass<typeof NavigatedViewer>;
-  //     expect(mockNavigatedViewer.mock.instances[0].importXML).toHaveBeenCalledWith(bpmnString);
-  //   });
-  // });
+  it("uses Viewer when navigated prop is false", async () => {
+    const bpmnString = "some-bpmn-xml-string";
+    render(<BpmnViewer bpmnString={bpmnString} navigated={false} />);
+
+    await waitFor(() => {
+      expect(Viewer).toHaveBeenCalled();
+    });
+  });
 });
