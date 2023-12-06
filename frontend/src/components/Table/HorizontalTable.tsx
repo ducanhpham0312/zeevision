5c50c42599efc35a85d30369e5bec292757be7e2
Duc Anh Pham <77518288+ducanhpham0312@users.noreply.github.com>
2023-12-06 19:03:29 +0200

    fix: table bugs (#268)
    Co-authored-by: Duy Tran <trananhduy041003@gmail.com>

--- frontend/src/components/Table/HorizontalTable.tsx
+++ frontend/src/components/Table/HorizontalTable.tsx
@@ -1,4 +1,11 @@
-import * as React from "react";
+import {
+  useState,
+  useCallback,
+  useEffect,
+  ReactNode,
+  MouseEvent,
+  ChangeEvent,
+} from "react";
 import { styled } from "@mui/system";
 import {
   TablePagination,
@@ -7,45 +14,49 @@ import {
 import { Button } from "../Button";
 import { Minus, Plus } from "lucide-react";
 import { ExpandRow } from "./ExpandRow";
+import { NavLink } from "react-router-dom";
 
 export interface HorizontalTableProps {
   header: string[];
-  content: (string | number | React.ReactNode)[][];
+  content: (string | number)[][];
+  navLinkColumn?: Record<string, (value: string | number) => string>;
   alterRowColor?: boolean;
-  expandElement?: (idx: number) => React.ReactNode;
-  optionElement?: (idx: number) => React.ReactNode;
+  expandElement?: (idx: number) => ReactNode;
+  optionElement?: (idx: number) => ReactNode;
 }
 
 export function HorizontalTable({
   header,
   content,
   alterRowColor,
+  navLinkColumn,
   expandElement,
   optionElement,
 }: HorizontalTableProps) {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [sortBy, setSortBy] = React.useState("");
-  const [sortOrder, setSortOrder] = React.useState("desc");
-  const [expandedRow, setExpandedRow] = React.useState<number | null>(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [sortBy, setSortBy] = useState("");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const [expandedRow, setExpandedRow] = useState<number | null>(null);
   const [sortedContent, setSortedContent] =
-    React.useState<(string | number | React.ReactNode)[][]>(content);
+    useState<(string | number)[][]>(content);
 
-  const sortContent = React.useCallback(
+  const sortContent = useCallback(
     (
-      content: (string | number | React.ReactNode)[][],
+      content: (string | number)[][],
       column: string,
       order: string,
-    ): (string | number | React.ReactNode)[][] => {
-      return content.slice().sort((a, b) => {
-        const comparison =
-          a[header.indexOf(column)]! > b[header.indexOf(column)]! ? 1 : -1;
+    ): (string | number)[][] => {
+      return [...content].sort((a, b) => {
+        const columnIndex = header.indexOf(column);
+        const comparison = a[columnIndex] > b[columnIndex] ? 1 : -1;
         return order === "desc" ? comparison * -1 : comparison;
       });
     },
     [header],
   );
-  React.useEffect(() => {
+
+  useEffect(() => {
     // Sort the content when sortBy or sortOrder changes
     const sortedData = sortContent(content, sortBy, sortOrder);
     setSortedContent(sortedData);
@@ -63,14 +74,14 @@ export function HorizontalTable({
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - content.length) : 0;
 
   const handleChangePage = (
-    _event: React.MouseEvent<HTMLButtonElement> | null,
+    _event: MouseEvent<HTMLButtonElement> | null,
     newPage: number,
   ) => {
     setPage(newPage);
   };
 
   const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -116,11 +127,19 @@ export function HorizontalTable({
                   }
                   key={rowIdx}
                 >
-                  {row.map((cell, index) => (
+                  {row.map((value, index) => (
                     <td className="p-3" key={index}>
-                      <p>
-                        {typeof cell === "string" ? prettifyJson(cell) : cell}
-                      </p>
+                      {navLinkColumn && navLinkColumn[header[index]] ? (
+                        <NavLink to={navLinkColumn[header[index]](value)}>
+                          <Button variant="secondary">{value}</Button>
+                        </NavLink>
+                      ) : (
+                        <p>
+                          {typeof value === "string"
+                            ? prettifyJson(value)
+                            : value}
+                        </p>
+                      )}
                     </td>
                   ))}
                   {expandElement ? (
